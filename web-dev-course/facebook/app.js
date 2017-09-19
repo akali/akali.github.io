e07722e7a3fa80d8054b0044a7e3d4adc0776487
Aisultan Kali <aisultan.kali@gmail.com>
2017-09-20 03:30:14 +0600

    facebook complete

--- web-dev-course/facebook/app.js
+++ web-dev-course/facebook/app.js
@@ -1,6 +1,8 @@
 window.onload = function() {
 	let d = document.getElementsByClassName("date");
 	let a = document.createElement("select");
+	a.style.height="30px";
+	a.style.paddingRight="5px";
 	for (let i = 2017; i >= 1905; --i) {
 		let b = document.createElement("option");
 		b.appendChild(document.createTextNode(i));
@@ -9,6 +11,8 @@ window.onload = function() {
 	d[0].appendChild(a);
 	let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 	a = document.createElement("select");
+	a.style.height="30px";
+	a.style.paddingRight="5px";
 	months.forEach((val) => {
 		let b = document.createElement("option");
 		b.appendChild(document.createTextNode(val));
@@ -16,19 +20,28 @@ window.onload = function() {
 	});
 	d[0].appendChild(a);
 	a = document.createElement("select");
+	a.style.height="30px";
+	a.style.paddingRight="5px";
 	for (let i = 1; i <= 31; ++i) {
 		let b = document.createElement("option");
 		b.appendChild(document.createTextNode(i));
 		a.appendChild(b);
 	}
 	d[0].appendChild(a);
+	let q = document.createElement('div');
+	q.style.width = "150px";
+	q.style.height="30px";
+	q.style.marginLeft= "10px";
+
 	a = document.createElement('a');
+	a.fontSize = "11px";
 	a.href='https://www.facebook.com/';
 	a.appendChild(document.createTextNode('Зачем указывать дату рождения?'));
-	a.class='whybday';
 	a.style.fontSize="10px";
 	a.style.textDecoration="none";
 	a.style.fontFamily="'Helvetica', 'Arial', sans-serif !important;";
-	
-	d[0].appendChild(a);
+
+	q.appendChild(a);
+
+	d[0].appendChild(q);
 }

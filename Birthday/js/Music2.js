a5d2c1897d01d1e6f339c261a53f4b60db2c58fc
Aisultan Kali <akali@Aisultans-MacBook-Air.local>
2023-05-28 09:55:44 +0200

    rebuilt with hugo

--- Birthday/js/Music2.js
+++
@@ -1,3 +0,0 @@
-var audioBir = new Audio ('http://upload.wikimedia.org/wikipedia/commons/f/f5/Good_Morning_to_All.ogg');
-var PlayerBir = audioBir;
-PlayerBir.play();
---
+++ hugo/content/projects/index.md
@@ -0,0 +1,6 @@
+---
+title: "Projects"
+date: 2023-05-28T09:16:38+02:00
+draft: true
+---
+
